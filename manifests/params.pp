6f1b4a2681af9cb459456abbcd735f60aff4143e
Taylor Leese <taylor@askleonardo.com>
2014-09-29 23:04:07 -0700

    overridable defaults for apache

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,12 +1,14 @@
 
 class puppetdb_rundeck::params {
-  $with_sinatra     = true
-  $approot         = '/etc/puppetdb_rundeck/'
-  $servername       = $::fqdn
-  $port             = '8888'
-  $puppetdb_host    = 'localhost'
-  $puppetdb_port    = '8080'
-
+  $with_sinatra         = true
+  $approot              = '/etc/puppetdb_rundeck'
+  $servername           = $::fqdn
+  $port                 = '8888'
+  $puppetdb_host        = 'localhost'
+  $puppetdb_port        = '8080'
+  $default_mods         = true
+  $default_vhost        = true
+  $default_confd_files  = true
 
   case $::osfamily {
     'redhat': {
